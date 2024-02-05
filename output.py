2cfef6a9e1a02c98ac6ed74b130411797b37e5a6
AdmirableEgg <63537547+AdmirableEgg@users.noreply.github.com>
2024-02-05 13:25:37 +0100

    txt_without_tc output option
    Added a txt_without_tc output option. Faster than adding a switch. A switch would only be useful for txt anyway, not csv or srt.

--- output.py
+++ output.py
@@ -3,7 +3,7 @@ import datetime as dt
 import timecode
 import csv
 
-output_options = ['', 'txt', 'csv', 'srt']
+output_options = ['', 'txt', 'txt without TC','csv', 'srt']
 
 def write_txt(audio_file, model_name, language, translation, result, start_time):
     now = dt.datetime.now()
@@ -14,6 +14,17 @@ def write_txt(audio_file, model_name, language, translation, result, start_time)
     output_file.write(f"Transcription performed on {now.strftime('%Y-%m-%d %H:%M:%S')}.\nLanguage: {language}\nModel: {model_name}\nTranslation: {translation}\n\n")
     for segment in result['segments']:
         output_file.write(f"[{timecode.time_to_TC(segment['start'] + start_time, 25)} - {timecode.time_to_TC(segment['end'] + start_time, 25)}]   > ")
+        output_file.write(f"{segment['text']}\n")
+    output_file.close()
+
+def write_txt_without_tc(audio_file, model_name, language, translation, result):
+    now = dt.datetime.now()
+    audio_filename = os.path.basename(audio_file)
+    dir_name = os.path.dirname(audio_file)
+    
+    output_file = open(os.path.join(dir_name, f"{now.strftime('%Y%m%d-%H%M%S')}_{audio_filename}_transcription.txt"), 'wt')
+    output_file.write(f"Transcription performed on {now.strftime('%Y-%m-%d %H:%M:%S')}.\nLanguage: {language}\nModel: {model_name}\nTranslation: {translation}\n\n")
+    for segment in result['segments']:
         output_file.write(f"{segment['text']}\n")
     output_file.close()
 
